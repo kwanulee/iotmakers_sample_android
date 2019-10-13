32abf450c214a2e9592ab39b2d55efcc4279e239
kwanulee <kwlee@hansung.ac.kr>
2019-10-14 00:29:54 +0900

    초기 커밋

---
+++ app/src/main/java/com/kt/iot/mobile/ui/activity/PolicyActivity.java
@@ -0,0 +1,48 @@
+package com.kt.iot.mobile.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kt.iot.mobile.android.R;
+import com.kt.iot.mobile.ui.fragment.policy.PolicyFragment;
+
+/**
+ * Created by ceoko on 15. 6. 16..
+ */
+public class PolicyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_fragment_common);
+
+        PolicyFragment fragment = new PolicyFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.layout_fragment_container, fragment).commit();
+
+        getSupportActionBar().setElevation(0);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+
+            case android.R.id.home:
+
+                finish();
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
